947b80144e2d20ae2cd151374412a97151d29f62
Jose Vigo <josevigodev@gmail.com>
2025-07-30 11:01:46 -0400

    add authentification with firebase

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -1,81 +1,95 @@
-export default function LogIn() {
-  return (
-    // <main className='flex-1 min-h-dvh mx-auto max-w-lg p-6'>
-    //   <form>
-    //     <h2 className='text-center text-2xl font-semibold mb-10 text-dark-text'>
-    //       Sign in/Register
-    //     </h2>
-    //     <div className='flex flex-col gap-5'>
-    //       <label htmlFor='email' className='flex-col flex text-gray-500'>
-    //         Email
-    //         <input
-    //           autoFocus
-    //           id='email'
-    //           type='email'
-    //           className='border-1 border-gray-400 text-dark-text p-2 px-3 focus-visible:outline-dark-gray focus-visible:outline-1 rounded-sm'
-    //         />
-    //       </label>
+'use client';
+import { signIn, signUp } from '@/firebase/client';
+import { useState } from 'react';
 
-    //       <button
-    //         type='submit'
-    //         className='bg-dark-gray text-white p-3 font-semibold cursor-pointer rounded-sm'
-    //       >
-    //         Continue
-    //       </button>
-    //     </div>
-    //     <small className='text-placeholder block text-pretty text-center mt-10'>
-    //       By continuing, you agree to our{' '}
-    //       <a href='' className='text-blue-600'>
-    //         Privacy & Cookie Policy
-    //       </a>{' '}
-    //       and{' '}
-    //       <a href='' className='text-blue-600'>
-    //         Terms & Conditions
-    //       </a>
-    //       .
-    //     </small>
-    //   </form>
-    // </main>
+export default function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showSignIn, setShowSignIn] = useState(true);
+  const [text, setText] = useState('');
+
+  const handleSignIn = (e: React.FormEvent) => {
+    e.preventDefault();
+    const cleanEmail = email.trim().toLowerCase();
+    signIn(cleanEmail, password)
+      .then(() => setText('Succesfull'))
+      .catch(() => setText('Wrong'));
+  };
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const cleanEmail = email.trim().toLowerCase();
+    signUp(cleanEmail, password)
+      .then(() => setText('Succesfull'))
+      .catch((e) => {
+        if (e) {
+          setText('Account already exists');
+        }
+      });
+  };
+
+  const handleChandeMode = () => {
+    setShowSignIn((prev) => !prev);
+    setEmail('');
+    setPassword('');
+    setText('');
+  };
+
+  return (
     <main className='flex-1 min-h-dvh mx-auto max-w-lg p-6'>
-      <div className='mb-4'>
-        <h2 className='font-semibold text-2xl text-dark-text'>So, your New!</h2>
-        <small className='text-placeholder text-sm'>
-          Set your password to create an account.
-        </small>
-      </div>
-      <div className='mb-4'>
-        <h3 className='text-placeholder text-md'>Email Address</h3>
-        <button className='bg-gray-200 text-placeholder font-semibold p-2 px-3 w-full text-start'>
-          josevigodev@gmail.com
-        </button>
-      </div>
-      <form className='flex flex-col gap-5'>
-        <label htmlFor='password' className='text-placeholder flex flex-col'>
-          Password
-          <input
-            id='password'
-            type='password'
-            className='border-1 border-gray-400 text-dark-text p-2 px-3 focus-visible:outline-dark-gray focus-visible:outline-1 rounded-sm'
-          />
-        </label>
-        <button
-          type='submit'
-          className='bg-dark-gray text-white p-3 font-semibold cursor-pointer rounded-sm'
-        >
-          Register
-        </button>
-        <small className='text-placeholder block text-pretty text-center mt-10 text-sm'>
-          By continuing, you agree to our{' '}
-          <a href='' className='text-blue-600'>
-            Privacy & Cookie Policy
-          </a>{' '}
-          and{' '}
-          <a href='' className='text-blue-600'>
-            Terms & Conditions
-          </a>
-          .
-        </small>
+      <form onSubmit={handleSignIn}>
+        <h2 className='text-center text-2xl font-semibold mb-10 text-dark-text'>
+          {showSignIn ? 'Sign in' : 'Register'}
+        </h2>
+        <div className='flex flex-col gap-5'>
+          <label htmlFor='email' className='flex-col flex text-gray-500'>
+            Email
+            <input
+              autoFocus
+              id='email'
+              type='email'
+              className='border-1 border-gray-400 text-dark-text p-2 px-3 focus-visible:outline-dark-gray focus-visible:outline-1 rounded-sm'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor='password' className='text-placeholder flex flex-col'>
+            Password
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id='password'
+              type='password'
+              className='border-1 border-gray-400 text-dark-text p-2 px-3 focus-visible:outline-dark-gray focus-visible:outline-1 rounded-sm'
+            />
+          </label>
+          <span className='text-red-500'>{text}</span>
+          <button
+            onClick={showSignIn ? handleSignIn : handleSignUp}
+            type='submit'
+            className='bg-dark-gray text-white p-3 font-semibold cursor-pointer rounded-sm'
+          >
+            {showSignIn ? 'Log in' : 'Register'}
+          </button>
+        </div>
       </form>
+      <button
+        onClick={handleChandeMode}
+        className='text-blue-600 block text-pretty text-center text-sm mt-10 mx-auto cursor-pointer'
+      >
+        {showSignIn ? "You're new? - Register" : 'Already have an account'}
+      </button>
+      <small className='text-placeholder block text-pretty text-center mt-10'>
+        By continuing, you agree to our{' '}
+        <a href='' className='text-blue-600'>
+          Privacy & Cookie Policy
+        </a>{' '}
+        and{' '}
+        <a href='' className='text-blue-600'>
+          Terms & Conditions
+        </a>
+        .
+      </small>
     </main>
   );
 }
---
+++ src/firebase/client.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWB59jI1kzEGIn3T9RsNN0Kuq82udUwIE',
+  authDomain: 'buy-buy-912f1.firebaseapp.com',
+  projectId: 'buy-buy-912f1',
+  storageBucket: 'buy-buy-912f1.firebasestorage.app',
+  messagingSenderId: '1021680460797',
+  appId: '1:1021680460797:web:7ccb1edc06edb7da5caf94',
+  measurementId: 'G-H9EYCRBHTV',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const signUp = async (email: string, password: string) => {
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signIn = async (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
