f49dc311f8a8f9ddd0f37a620d77ddfb12725d27
Jose Vigo <josevigodev@gmail.com>
2025-08-19 18:24:03 -0400

    add increase and decrease functionality

--- src/store/shoppingCart.ts
+++ src/store/shoppingCart.ts
@@ -1,25 +1,60 @@
 import { Product } from '@/types/fakeStoreApi';
+import { getLocalStorage, updateLocalStorage } from '@/utils/localStorage';
 import { create } from 'zustand';
 
 interface ShoppingCart {
   cart: Product[];
-  addItem: ({ item }: { item?: Product }) => void;
+  addItem: ({ item }: { item: Product }) => void;
   deleteItem: ({ itemId }: { itemId: number }) => void;
+  increaseItemQty: ({ itemId }: { itemId: number }) => void;
+  decreaseItemQty: ({ itemId }: { itemId: number }) => void;
 }
 
 export const useShoppingCartStore = create<ShoppingCart>((set) => ({
-  cart: [],
+  cart: getLocalStorage('cart') || [],
   addItem: ({ item }) => {
-    if (item) {
-      set((state) => {
-        const cart = [...state.cart, item];
-        return { cart };
-      });
-    }
+    set((state) => {
+      const cart = [...state.cart, { ...item, qty: 1 }];
+      updateLocalStorage('cart', cart);
+      return { cart };
+    });
   },
   deleteItem: ({ itemId }) => {
     set((state) => {
       const cart = state.cart.filter((item) => item.id !== itemId);
+      updateLocalStorage('cart', cart);
+      return { cart };
+    });
+  },
+  increaseItemQty: ({ itemId }) => {
+    set((state) => {
+      const cart = state.cart.map((item) => {
+        if (item.id === itemId && item.qty) {
+          return {
+            ...item,
+            qty: item.qty + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+      updateLocalStorage('cart', cart);
+      return { cart };
+    });
+  },
+  decreaseItemQty: ({ itemId }) => {
+    set((state) => {
+      const cart = state.cart.map((item) => {
+        if (item.id === itemId && item.qty && item.qty > 1) {
+          return {
+            ...item,
+            qty: item.qty - 1,
+          };
+        } else {
+          return item;
+        }
+      });
+      updateLocalStorage('cart', cart);
       return { cart };
     });
   },
