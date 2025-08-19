1f29f0c5fbd5edee0c80a54ff5398fc1938ec125
Jose Vigo <josevigodev@gmail.com>
2025-08-19 15:22:41 -0400

    map products from json

--- src/app/marcket/page.tsx
+++ src/app/marcket/page.tsx
@@ -1,29 +1,36 @@
 import { Filters } from '@/components/Filters';
 import { ProductCard } from '@/components/ProductCard';
+import { products } from '@/mocks/products.json';
 
-export default function Marcket() {
+export default async function Marcket() {
+  // const response = await fetch('https://fakestoreapi.in/api/products?limit=23');
+  // const data = (await response.json()) as FakeStoreAPIResponse;
+  // const products = data.products;
+
+  const mappedItems = [...products]?.map((item) => ({
+    id: item.id,
+    title: item.title,
+    image: item.image,
+    price: item.price,
+    description: item.description,
+    brand: item.brand,
+    model: item.model,
+    color: item.color,
+    category: item.category,
+    discount: item.discount,
+    popular: item.popular,
+    onSale: item.onSale,
+  }));
+
   return (
     <main className='flex-1 mb-20 min-h-dvh px-3 lg:flex'>
       <Filters />
       <section className='grid gap-2 lg:grid-cols-4 flex-1 mt-5 lg:pl-3'>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
-        <div className='border-1 border-product-frame'>
-          <ProductCard />
-        </div>
+        {mappedItems.map((item) => (
+          <div key={item.id} className='border-1 border-product-frame'>
+            <ProductCard {...item} item={item} />
+          </div>
+        ))}
       </section>
     </main>
   );
