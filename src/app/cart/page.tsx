0591e666d0a266ab5eed0405918281b1dd95b700
Jose Vigo <josevigodev@gmail.com>
2025-08-19 18:20:58 -0400

    add total price interface

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,13 @@
-import { ProductCard } from '../../components/ProductCard';
-import ProductInCart from '../../components/ProductInCart';
+'use client';
+import { useShoppingCartStore } from '@/store/shoppingCart';
+import { Cart } from '../../components/Cart';
 export default function CartPage() {
+  const cart = useShoppingCartStore((state) => state.cart);
+
+  const totalPrice = cart.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+
   return (
     <main className='flex-1 mb-20 px-3'>
       <section className='sticky top-0 flex flex-col border-b-1 border-b-gray-400 gap-5 py-2 bg-white lg:flex-row lg:justify-between'>
@@ -15,17 +22,12 @@ export default function CartPage() {
             <input className='size-4' id='select' type='checkbox' />
             Select All
           </label>
-          <span className='text-dark-text'>Total: $323 (1 item)</span>
+          <span className='text-dark-text'>
+            Total: ${totalPrice || 0} ({cart.length} item)
+          </span>
         </div>
       </section>
-      <section className='grid gap-2 lg:grid-cols-3 xl:grid-cols-4 flex-1 mt-5'>
-        <ProductInCart>
-          <ProductCard />
-        </ProductInCart>
-        <ProductInCart>
-          <ProductCard />
-        </ProductInCart>
-      </section>
+      <Cart />
     </main>
   );
 }
