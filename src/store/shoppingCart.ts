487a489ae5ff2c1db3bf2874a3d486d7c2ea3f70
Jose Vigo <josevigodev@gmail.com>
2025-08-24 14:57:51 -0400

    add persistance for localStorage with zustand

--- src/store/shoppingCart.ts
+++ src/store/shoppingCart.ts
@@ -1,6 +1,6 @@
 import { Product } from '@/types/fakeStoreApi';
-import { getLocalStorage, updateLocalStorage } from '@/utils/localStorage';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface ShoppingCart {
   cart: Product[];
@@ -10,52 +10,60 @@ interface ShoppingCart {
   decreaseItemQty: ({ itemId }: { itemId: number }) => void;
 }
 
-export const useShoppingCartStore = create<ShoppingCart>((set) => ({
-  cart: getLocalStorage('cart') || [],
-  addItem: ({ item }) => {
-    set((state) => {
-      const cart = [...state.cart, { ...item, qty: 1 }];
-      updateLocalStorage('cart', cart);
-      return { cart };
-    });
-  },
-  deleteItem: ({ itemId }) => {
-    set((state) => {
-      const cart = state.cart.filter((item) => item.id !== itemId);
-      updateLocalStorage('cart', cart);
-      return { cart };
-    });
-  },
-  increaseItemQty: ({ itemId }) => {
-    set((state) => {
-      const cart = state.cart.map((item) => {
-        if (item.id === itemId && item.qty) {
-          return {
-            ...item,
-            qty: item.qty + 1,
-          };
-        } else {
-          return item;
-        }
-      });
-      updateLocalStorage('cart', cart);
-      return { cart };
-    });
-  },
-  decreaseItemQty: ({ itemId }) => {
-    set((state) => {
-      const cart = state.cart.map((item) => {
-        if (item.id === itemId && item.qty && item.qty > 1) {
-          return {
-            ...item,
-            qty: item.qty - 1,
-          };
-        } else {
-          return item;
-        }
-      });
-      updateLocalStorage('cart', cart);
-      return { cart };
-    });
-  },
-}));
+export const useShoppingCartStore = create<ShoppingCart>()(
+  persist(
+    (set) => {
+      return {
+        cart: [],
+        addItem: ({ item }) => {
+          set((state) => {
+            const cart = [
+              ...state.cart,
+              { ...item, qty: 1, isSelected: false },
+            ];
+            return { cart };
+          });
+        },
+        deleteItem: ({ itemId }) => {
+          set((state) => {
+            const cart = state.cart.filter((item) => item.id !== itemId);
+            return { cart };
+          });
+        },
+        increaseItemQty: ({ itemId }) => {
+          set((state) => {
+            const cart = state.cart.map((item) => {
+              if (item.id === itemId && item.qty) {
+                return {
+                  ...item,
+                  qty: item.qty + 1,
+                };
+              } else {
+                return item;
+              }
+            });
+            return { cart };
+          });
+        },
+        decreaseItemQty: ({ itemId }) => {
+          set((state) => {
+            const cart = state.cart.map((item) => {
+              if (item.id === itemId && item.qty && item.qty > 1) {
+                return {
+                  ...item,
+                  qty: item.qty - 1,
+                };
+              } else {
+                return item;
+              }
+            });
+            return { cart };
+          });
+        },
+      };
+    },
+    {
+      name: 'cart',
+    }
+  )
+);
