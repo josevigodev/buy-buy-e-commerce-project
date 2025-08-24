3a5b92ac52d5f1789e9a584b3b7530cc7fadd33f
Jose Vigo <josevigodev@gmail.com>
2025-08-24 14:56:37 -0400

    add conditional styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import { Rating } from './Rating';
 import { Product } from '@/types/fakeStoreApi';
-import { useShoppingCartStore } from '@/store/shoppingCart';
+// import { useShoppingCartStore } from '@/store/shoppingCart';
 import { usePathname } from 'next/navigation';
 
 export const ProductCard: React.FC<Product> = function ({
@@ -10,17 +10,23 @@ export const ProductCard: React.FC<Product> = function ({
   image = '/computer.png',
   price,
   item,
+  qty,
 }) {
-  const isShoppingCart = usePathname() === '/cart';
-  const addItem = useShoppingCartStore((state) => state.addItem);
+  // const isShoppingCart = usePathname() === '/cart';
+  // const addItem = useShoppingCartStore((state) => state.addItem);
 
-  const handleAddToCart = () => {
-    addItem({ item });
-  };
+  // const handleAddToCart = () => {
+  //   addItem({ item });
+  // };
 
+  const isPlaceOrder = usePathname() === '/place-order';
   return (
-    <article className='flex rounded-sm lg:flex-col'>
-      <div className='w-1/3 p-2 pt-0 flex items-center justify-center bg-white lg:w-full'>
+    <article className={`flex rounded-sm ${isPlaceOrder ? '' : 'lg:flex-col'}`}>
+      <div
+        className={`w-1/3 p-2 pt-0 flex items-center justify-center bg-white ${
+          isPlaceOrder ? '' : 'lg:w-full'
+        }`}
+      >
         <Image width={200} height={200} src={image} alt={title}></Image>
       </div>
       <div className='flex-1 p-2 pt-0 flex flex-col text-dark-text gap-1 md:p-3'>
@@ -34,14 +40,15 @@ export const ProductCard: React.FC<Product> = function ({
           <strong className='font-normal text-md mr-0.5'>$</strong>
           {price}
         </span>
-        {isShoppingCart || (
+        {/* {isShoppingCart || (
           <button
             className='className=absolute bottom-1/4 left-0 right-0 text-center mx-auto w-fit px-4 py-2 bg-dark-gray text-white text-md font-semibold rounded-full shadow-xl hover:bg-white hover:text-dark-gray border-2 border-dark-gray transition duration-300 flex items-center gap-1.5 md:text-lg md:px-6 md-py-4'
             onClick={handleAddToCart}
           >
             add to cart
           </button>
-        )}
+        )} */}
+        <span>x{qty}</span>
       </div>
     </article>
   );
