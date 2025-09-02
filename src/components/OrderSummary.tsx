b54403d3d20656a1ef1e42259149f5eae800728b
Jose Vigo <josevigodev@gmail.com>
2025-09-02 14:03:52 -0400

    implement page navigation in case the form is filled correctly

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -2,10 +2,56 @@
 import { useShoppingCartStore } from '@/store/shoppingCart';
 import { ProductCard } from './ProductCard';
 import { useCartInfo } from '@/hooks/useCartInfo';
+import Link from 'next/link';
+import { useShippingForm } from '@/store/shippingForm';
+import { useEffect, useState } from 'react';
+import { usePaymentForm } from '@/store/paymentForm';
+import { usePathname } from 'next/navigation';
 
 export function OrderSummary() {
   const cart = useShoppingCartStore((state) => state.cart);
   const { totalPrice } = useCartInfo();
+  const shippingForm = useShippingForm((state) => state.shippingForm);
+  const paymentForm = usePaymentForm((state) => state.paymentForm);
+  const [isValid, setIsValid] = useState({
+    shippingForm: false,
+    paymentForm: false,
+  });
+
+  const isShippingForm = usePathname() === '/checkout';
+  const isPaymentForm = usePathname() === '/payment';
+
+  useEffect(() => {
+    setIsValid((state) => {
+      if (shippingForm.firstName) {
+        return {
+          ...state,
+          shippingForm: true,
+        };
+      } else {
+        return {
+          ...state,
+          shippingForm: false,
+        };
+      }
+    });
+  }, [shippingForm]);
+
+  useEffect(() => {
+    setIsValid((state) => {
+      if (paymentForm.name) {
+        return {
+          ...state,
+          paymentForm: true,
+        };
+      } else {
+        return {
+          ...state,
+          paymentForm: false,
+        };
+      }
+    });
+  }, [paymentForm]);
   return (
     <>
       <h2 className='font-bold text-dark-text text-xl xl:text-2xl'>
@@ -20,6 +66,29 @@ export function OrderSummary() {
           <ProductCard key={item.id} {...item} />
         ))}
       </section>
+      {isShippingForm ? (
+        <Link
+          href={'/payment'}
+          className={`bg-dark-gray text-white p-3 font-semibold rounded-sm mt-3 text-center ${
+            isValid.shippingForm
+              ? 'cursor-pointer'
+              : 'cursor-default opacity-70 pointer-events-none'
+          }`}
+        >
+          Place order
+        </Link>
+      ) : isPaymentForm ? (
+        <Link
+          href={'/order-confirmed'}
+          className={`bg-dark-gray text-white p-3 font-semibold rounded-sm mt-3 text-center ${
+            isValid.paymentForm
+              ? 'cursor-pointer'
+              : 'cursor-default opacity-70 pointer-events-none'
+          }`}
+        >
+          Confirm your order
+        </Link>
+      ) : null}
     </>
   );
 }
