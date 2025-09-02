3f692a60567ba50845e6565915396fdfdcb6f8eb
Jose Vigo <josevigodev@gmail.com>
2025-09-02 14:05:59 -0400

    create store for shipping form

---
+++ src/store/shippingForm.ts
@@ -0,0 +1,40 @@
+import { ShippingFormInputs } from '@/components/ShippingForm';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { ShippingForm } from '../components/ShippingForm';
+
+interface ShippingForm {
+  shippingForm: ShippingFormInputs;
+  updateShippingForm: ({ data }: { data: ShippingFormInputs }) => void;
+}
+
+const emptyShippingForm = {
+  firstName: '',
+  lastName: '',
+  country: '',
+  phoneNumber: '',
+  city: '',
+  state: '',
+  postCode: '',
+  addressLine1: '',
+  addressLine2: '',
+};
+
+export const useShippingForm = create<ShippingForm>()(
+  persist(
+    (set) => {
+      return {
+        shippingForm: emptyShippingForm,
+        updateShippingForm: ({ data }) => {
+          set(() => {
+            const shippingForm = data;
+            return { shippingForm };
+          });
+        },
+      };
+    },
+    {
+      name: 'shippingForm',
+    }
+  )
+);
