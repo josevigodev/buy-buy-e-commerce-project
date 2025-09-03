a12649fda7ce1ae39ebb64fc58a1cc679658a6fe
Jose Vigo <josevigodev@gmail.com>
2025-09-03 18:46:12 -0400

    fix to show the total price of the products in cart

--- src/hooks/useCartInfo.ts
+++ src/hooks/useCartInfo.ts
@@ -1,11 +1,22 @@
 import { useShoppingCartStore } from '@/store/shoppingCart';
+import { products } from '@/mocks/products.json';
 
 export function useCartInfo() {
   const cart = useShoppingCartStore((state) => state.cart);
 
-  const totalPrice = cart.reduce((acc, item) => {
-    return (acc += item.price * (item.qty || 1));
-  }, 0);
+  const productsInCart = cart.map((item) => {
+    return products.find((product) => product.id === item.id);
+  });
+
+  const totalPrice = cart.map((cartItem) => {
+    return productsInCart.reduce((acc, item) => {
+      if (item) {
+        return (acc += item.price * (cartItem.qty || 1));
+      } else {
+        return acc;
+      }
+    }, 0);
+  });
 
   const itemsQty = cart.reduce((acc, item) => {
     if (item.qty) return (acc += item.qty);
