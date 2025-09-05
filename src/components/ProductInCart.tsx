9fe394c86005226e644d665c0bc0f9846d3f5ed2
Jose Vigo <josevigodev@gmail.com>
2025-09-05 08:23:31 -0400

    change styles

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -25,7 +25,7 @@ export const ProductInCart: React.FC<Props> = ({
   const disabled = 'opacity-50 pointer-events-none';
 
   return (
-    <div className='border-1 border-product-frame bg-product-frame rounded-sm pb-3'>
+    <div className='border-1 border-product-frame rounded-md bg-white pb-3 overflow-hidden'>
       {children}
       <div className='flex items-center gap-2 px-3 text-dark-text justify-end lg:justify-center'>
         <div className='flex rounded-sm border-1 border-gray-400'>
