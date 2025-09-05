277541f64054c7f77489b7753819acaab72daf62
Jose Vigo <josevigodev@gmail.com>
2025-09-05 08:21:40 -0400

    add title in case the cart is empty and disable the checkout button in case the cart is empty

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -11,17 +11,31 @@ export default function CartPage() {
       <section className='sticky top-0 flex flex-col border-b-1 border-b-gray-400 gap-5 py-2 bg-white lg:flex-row lg:justify-between'>
         <Link
           href='/checkout'
-          className='bg-dark-gray text-white rounded-sm p-2 cursor-pointer lg:order-2'
+          aria-disabled={itemsQty < 1}
+          className={`bg-dark-gray text-white rounded-sm p-2 lg:order-2 ${
+            itemsQty < 1
+              ? 'pointer-events-none cursor-default opacity-70'
+              : 'cursor-pointer'
+          }`}
         >
           Checkout ({itemsQty} {itemsQty > 1 ? 'items' : 'item'})
         </Link>
         <div className='flex items-center justify-between flex-1'>
-          <span className='text-dark-text'>
-            Total: ${totalPrice || 0} ({itemsQty} item)
+          <span className='text-dark-text text-lg'>
+            <strong>Total</strong>: ${totalPrice || '0'} ({itemsQty}{' '}
+            {itemsQty > 1 ? 'items' : 'item'})
           </span>
         </div>
       </section>
-      <Cart />
+      {itemsQty < 1 ? (
+        <section className='flex items-center justify-center h-150'>
+          <h2 className='text-2xl text-dark-gray font-semibold text-center pointer-events-none'>
+            Add products to the shopping cart so you{"'"}ll see them here
+          </h2>
+        </section>
+      ) : (
+        <Cart />
+      )}
     </main>
   );
 }
