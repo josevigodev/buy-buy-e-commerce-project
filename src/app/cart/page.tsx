681b3d8ecfd1da91b2ce5c8ad83c89dc512e950f
Jose Vigo <josevigodev@gmail.com>
2025-09-05 17:33:58 -0400

    change style

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,27 +5,26 @@ import Link from 'next/link';
 
 export default function CartPage() {
   const { totalPrice, itemsQty } = useCartInfo();
+  const disabled = 'opacity-50 pointer-events-none';
 
   return (
-    <main className='flex-1 mb-20 px-3'>
-      <section className='sticky top-0 flex flex-col border-b-1 border-b-gray-400 gap-5 py-2 bg-white lg:flex-row lg:justify-between'>
-        <Link
-          href='/checkout'
-          aria-disabled={itemsQty < 1}
-          className={`bg-dark-gray text-white rounded-sm p-2 lg:order-2 ${
-            itemsQty < 1
-              ? 'pointer-events-none cursor-default opacity-70'
-              : 'cursor-pointer'
-          }`}
-        >
-          Checkout ({itemsQty} {itemsQty > 1 ? 'items' : 'item'})
-        </Link>
+    <main className='flex-1 mb-20 px-3 min-h-dvh'>
+      <section className='sticky top-0 flex border-b-1 border-b-gray-400 gap-5 py-2 bg-white lg:flex-row lg:justify-between'>
         <div className='flex items-center justify-between flex-1'>
           <span className='text-dark-text text-lg'>
             <strong>Total</strong>: ${totalPrice || '0'} ({itemsQty}{' '}
             {itemsQty > 1 ? 'items' : 'item'})
           </span>
         </div>
+        <Link
+          href='/checkout'
+          aria-disabled={itemsQty < 1}
+          className={`bg-dark-gray text-white rounded-sm p-2 ${
+            itemsQty < 1 && disabled
+          }`}
+        >
+          Checkout ({itemsQty} {itemsQty > 1 ? 'items' : 'item'})
+        </Link>
       </section>
       {itemsQty < 1 ? (
         <section className='flex items-center justify-center h-150'>
