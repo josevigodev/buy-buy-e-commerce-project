c4870b8058c8ab688361003ea88e8cb54e384850
Jose Vigo <josevigodev@gmail.com>
2025-09-06 11:36:12 -0400

    create component

---
+++ src/components/ProductInWishes.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { useShoppingCartStore } from '@/store/shoppingCart';
+import { useWishesStore } from '@/store/wishes';
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  itemId: number;
+}
+
+export const ProductInWishes: React.FC<Props> = ({ children, itemId }) => {
+  const addItem = useShoppingCartStore((state) => state.addItem);
+  const deleteItemFromWishes = useWishesStore(
+    (state) => state.deleteItemFromWishes
+  );
+
+  const handleAddItemToWishesClick = () => {
+    addItem({ itemId });
+    deleteItemFromWishes({ itemId });
+  };
+
+  const handleDeleteItemFromWishesClick = () => {
+    deleteItemFromWishes({ itemId });
+  };
+  return (
+    <div className='border-1 h-fit pt-1 border-product-frame rounded-md bg-white pb-3 overflow-hidden'>
+      {children}
+      <div className='flex items-center gap-2 px-3 text-dark-text justify-end lg:justify-center'>
+        <div className='flex rounded-sm border-1 border-gray-400'></div>
+        <button
+          onClick={handleAddItemToWishesClick}
+          className='cursor-pointer rounded-sm border-1 border-gray-400 bg-white p-1 px-3 text-nowrap'
+        >
+          Add to cart
+        </button>
+        <button
+          onClick={handleDeleteItemFromWishesClick}
+          className='cursor-pointer rounded-sm border-1 border-gray-400 bg-white p-1 px-3'
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
