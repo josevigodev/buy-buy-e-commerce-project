0bb2a7583586cb3df7aa0337bab95d3df6713022
Jose Vigo <josevigodev@gmail.com>
2025-09-07 10:36:28 -0400

    show pop up when click add to cart button

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { useShoppingCartStore } from '@/store/shoppingCart';
+import { PopUp } from './PopUp';
+import { useEffect, useState } from 'react';
 
 interface Props {
   itemId: number;
@@ -7,19 +9,35 @@ interface Props {
 }
 
 export const AddToCartButton: React.FC<Props> = ({ itemId, isMobile }) => {
+  const [showPopUp, setShowPopUp] = useState(false);
   const addItem = useShoppingCartStore((state) => state.addItem);
 
   const handleAddToCartClick = () => {
     addItem({ itemId });
+    setShowPopUp(true);
   };
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (showPopUp) {
+        setShowPopUp(false);
+      }
+    }, 2000);
+
+    return () => clearTimeout(id);
+  }, [showPopUp]);
+
   return (
-    <button
-      onClick={handleAddToCartClick}
-      className={`w-fit px-4 py-2 bg-dark-text text-white text-md font-semibold rounded-full shadow-xl hover:bg-white hover:text-dark-text border-2 border-dark-text transition duration-300 cursor-pointer md:text-lg ${
-        isMobile ? 'lg:hidden' : 'hidden lg:block'
-      }`}
-    >
-      Add to cart
-    </button>
+    <>
+      {showPopUp && <PopUp text='Product added to shopping cart' />}
+      <button
+        onClick={handleAddToCartClick}
+        className={`w-fit px-4 py-2 bg-dark-text text-white text-md font-semibold rounded-full shadow-xl hover:bg-white hover:text-dark-text border-2 border-dark-text transition duration-300 cursor-pointer md:text-lg ${
+          isMobile ? 'lg:hidden' : 'hidden lg:block'
+        }`}
+      >
+        Add to cart
+      </button>
+    </>
   );
 };
