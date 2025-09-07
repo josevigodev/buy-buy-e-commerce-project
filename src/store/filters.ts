ee330796e7cef20bcdcd95765f7b096a245f9932
Jose Vigo <josevigodev@gmail.com>
2025-09-06 20:54:10 -0400

    craete filters store

---
+++ src/store/filters.ts
@@ -0,0 +1,56 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface Filters {
+  category: string | null;
+  brand: string[];
+  search: string;
+}
+
+interface FilterStore {
+  filters: Filters;
+  setFilters: ({
+    key,
+    value,
+  }: {
+    key: keyof Filters;
+    value: string | string[];
+  }) => void;
+  resetFilters: () => void;
+}
+
+export const useFilterStore = create<FilterStore>()(
+  persist(
+    (set) => {
+      return {
+        filters: {
+          category: null,
+          brand: [],
+          search: '',
+        },
+        setFilters: ({ key, value }) => {
+          set((state) => {
+            return {
+              filters: {
+                ...state.filters,
+                [key]: value,
+              },
+            };
+          });
+        },
+        resetFilters: () => {
+          set(() => ({
+            filters: {
+              category: null,
+              brand: [],
+              search: '',
+            },
+          }));
+        },
+      };
+    },
+    {
+      name: 'filters',
+    }
+  )
+);
