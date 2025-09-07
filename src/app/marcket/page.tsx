6ba8d8f2faabbcc6ab4cb739932dbc7a6a0bc6f0
Jose Vigo <josevigodev@gmail.com>
2025-09-06 20:49:13 -0400

    add filters

--- src/app/marcket/page.tsx
+++ src/app/marcket/page.tsx
@@ -1,8 +1,12 @@
+'use client';
 import { Filters } from '@/components/Filters';
 import { ProductCard } from '@/components/ProductCard';
 import { products } from '@/mocks/products.json';
+import { useFilterStore } from '@/store/filters';
+import { slugify } from '@/utils/slugify';
+import Link from 'next/link';
 
-export default async function Marcket() {
+export default function Marcket() {
   // const response = await fetch('https://fakestoreapi.in/api/products?limit=23');
   // const data = (await response.json()) as FakeStoreAPIResponse;
   // const products = data.products;
@@ -22,14 +26,35 @@ export default async function Marcket() {
     onSale: item.onSale,
   }));
 
+  const filters = useFilterStore((state) => state.filters);
+
+  const filteredProducts = mappedItems
+    .filter((item) =>
+      filters.search
+        ? item.title.toLowerCase().includes(filters.search.toLowerCase())
+        : true
+    )
+    .filter((item) =>
+      filters.category ? item.category === filters.category : true
+    );
+
   return (
     <main className='flex-1 mb-20 min-h-dvh px-3 lg:flex'>
       <Filters />
+      {filteredProducts.length < 1 && (
+        <h2 className='text-center text-2xl font-bold text-gray-500 w-full mt-20'>
+          No matched products found
+        </h2>
+      )}
       <section className='grid gap-2 lg:grid-cols-4 flex-1 mt-5 lg:pl-3'>
-        {mappedItems.map((item) => (
-          <div key={item.id} className='border-1 border-product-frame flex'>
+        {filteredProducts.map((item) => (
+          <Link
+            href={`/products/${slugify(item.title)}`}
+            key={item.id}
+            className='border-1 border-product-frame flex h-fit'
+          >
             <ProductCard {...item} />
-          </div>
+          </Link>
         ))}
       </section>
     </main>
