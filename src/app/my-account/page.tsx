1b412505289a24166c53d8d856b6c1578d912465
Jose Vigo <josevigodev@gmail.com>
2025-09-13 16:40:16 -0400

    add account info and user's passed orders

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,10 +1,68 @@
+'use client';
+import { useEffect, useState } from 'react';
 import ProtectedClient from '@/components/ProtectedClient';
 import { SignOutButton } from '@/components/SignOutButton';
+import { useAuthStore } from '@/store/user';
+import { getOrders, UserOrder } from '@/services/order';
+
+interface DBOrder extends UserOrder {
+  id: string;
+}
 
 export default function MyAccountPage() {
+  const user = useAuthStore((state) => state.user);
+  const [orders, setOrders] = useState<DBOrder[]>();
+
+  useEffect(() => {
+    if (user) {
+      const orders = getOrders(user?.uid);
+      orders.then((res) => setOrders(res));
+    }
+  }, [user]);
+
   return (
     <main className='flex-1 min-h-screen mb-20 px-3 lg:px-5 bg-white pt-3'>
       <ProtectedClient>
+        <h1 className='text-3xl font-bold mb-6'>My Account</h1>
+        <div className='flex flex-col md:flex-row gap-3.5 mx-auto max-w-7xl'>
+          <section className='mb-8 bg-gray-100 p-4 rounded-xl h-fit md:order-1'>
+            <h2 className='text-xl font-semibold mb-2'>Account Info</h2>
+            <p className='text-gray-700'>Email: {user?.email}</p>
+          </section>
+          <section className='bg-gray-100 p-4 rounded-xl max-w-2xl flex-1'>
+            <h2 className='text-xl font-semibold mb-4'>My Orders</h2>
+            {orders && orders.length > 0 ? (
+              <ul className='space-y-4'>
+                {orders.map((order) => (
+                  <li key={order.id} className='border p-4 rounded-lg bg-white'>
+                    <p className='text-sm text-gray-500'>
+                      Order ID: {order.id}
+                    </p>
+                    <p className='text-lg font-bold'>Total: ${order.total}</p>
+                    <p className='text-sm text-gray-700'>
+                      Status: {order.status}
+                    </p>
+                    <div className='mt-2 space-y-1'>
+                      {order.items.map((item) => (
+                        <div
+                          key={item.id}
+                          className='flex gap-2 justify-between text-sm'
+                        >
+                          <span>
+                            {item.title} x{item.qty}
+                          </span>
+                          <span>${item.price}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className='text-gray-600'>You have no orders yet.</p>
+            )}
+          </section>
+        </div>
         <SignOutButton />
       </ProtectedClient>
     </main>
