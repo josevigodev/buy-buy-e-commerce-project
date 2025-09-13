c8530605c25331bcfe65b886903a0c29022aa032
Jose Vigo <josevigodev@gmail.com>
2025-09-13 16:42:46 -0400

    create component

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { signOut } from 'firebase/auth';
+import { DoorExit } from './Icons';
+import { auth } from '@/firebase/client';
+import { redirect } from 'next/navigation';
+
+export function SignOutButton() {
+  const handleClick = () => {
+    signOut(auth);
+    redirect('/');
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className='flex items-center justify-between px-4 py-2 bg-dark-text text-white text-md font-semibold rounded-lg shadow-xl border-2 border-dark-text cursor-pointer '
+    >
+      <span>Sign out</span>
+      <DoorExit />
+    </button>
+  );
+}
