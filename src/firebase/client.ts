9cbf886cf37c2c80525fef37c93c2f003995c1e1
Jose Vigo <josevigodev@gmail.com>
2025-09-15 10:31:23 -0400

    implement env variables

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -7,12 +7,12 @@ import {
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCWB59jI1kzEGIn3T9RsNN0Kuq82udUwIE',
-  authDomain: 'buy-buy-912f1.firebaseapp.com',
-  projectId: 'buy-buy-912f1',
-  storageBucket: 'buy-buy-912f1.firebasestorage.app',
-  messagingSenderId: '1021680460797',
-  appId: '1:1021680460797:web:7ccb1edc06edb7da5caf94',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: 'G-H9EYCRBHTV',
 };
 
