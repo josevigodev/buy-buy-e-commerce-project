33f8c81cbe9a13a767e688fe4eefb4e5a67299b8
Jose Vigo <josevigodev@gmail.com>
2025-09-15 10:29:43 -0400

    delete test

--- cypress/e2e/signin.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('user login journey', () => {
-  it('should be able to sign in', () => {
-    cy.visit('http://localhost:3000');
-    cy.loginByFirebase();
-    cy.getByData('signin-link').contains('Hello josevigodev@gmail.com!');
-  });
-});
