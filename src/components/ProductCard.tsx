ac09c1f84b2993f8643c1e46cede6e4c5b3b08f8
Jose Vigo <josevigodev@gmail.com>
2025-09-25 19:54:58 -0400

    style: enhance ProductCard component with improved layout and functionality, including AddToCartButton and dynamic title rendering

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,19 @@
 'use client';
 import Image from 'next/image';
-import { Rating } from './Rating';
 import { usePathname } from 'next/navigation';
+import { AddToCartButton } from './AddToCartButton';
+import { slugify } from '@/utils/slugify';
+import Link from 'next/link';
+import { AddToWishListButton } from './AddToWishListButton';
 
 interface Props {
   title: string;
   image: string;
   price: number;
   qty?: number;
+  id: number;
+  brand: string;
+  model: string;
 }
 
 export const ProductCard: React.FC<Props> = function ({
@@ -15,35 +21,73 @@ export const ProductCard: React.FC<Props> = function ({
   image = '/computer.png',
   price,
   qty,
+  id,
+  brand,
+  model,
 }) {
   const isCheckout = usePathname() === '/checkout';
   const isPayment = usePathname() === '/payment';
   const isOrderConfirmed = usePathname() === '/order-confirmed';
+  const isCart = usePathname() === '/cart';
+  const isWishes = usePathname() === '/wishes';
 
   return (
     <article
-      className={`flex rounded-sm ${
-        isCheckout || isPayment || isOrderConfirmed ? '' : 'lg:flex-col'
-      }`}
+      className={`max-w-sm w-full relative bg-white rounded-2xl overflow-hidden h-fit ${
+        isCheckout ||
+        isPayment ||
+        isOrderConfirmed ||
+        isCart ||
+        isWishes ||
+        'shadow-lg pb-10 transform transition-all hover:-translate-y-2 hover:shadow-2xl'
+      } ${isCheckout || isPayment || isOrderConfirmed ? 'flex' : ''}`}
     >
       <div
-        className={`w-1/3 p-2 pt-0 flex items-center justify-center bg-white ${
-          isCheckout || isPayment || isOrderConfirmed ? '' : 'lg:w-full'
+        className={`relative p-2 pt-0 flex items-center justify-center bg-white ${
+          isCheckout || isPayment || isOrderConfirmed ? 'w-1/3 p-0' : ''
         }`}
+        style={{
+          background:
+            isCheckout || isPayment || isOrderConfirmed
+              ? ''
+              : 'linear-gradient(135deg, #0007 0%, #4a556577 50%, #fdc70077 100%)',
+        }}
       >
-        <Image width={200} height={200} src={image} alt={title}></Image>
+        <Image
+          src={image}
+          alt={title}
+          width={200}
+          height={200}
+          className='object-cover mix-blend-multiply opacity-95'
+        />
+        {isCheckout || isPayment || isOrderConfirmed || (
+          <AddToWishListButton itemId={id} />
+        )}
       </div>
-      <div className='flex-1 p-2 pt-0 flex flex-col text-dark-text gap-1 md:p-3'>
-        <p className='line-clamp-4 text-balance '>{title}</p>
-        <div className='flex items-center gap-1 text-sm'>
-          <span>4.2</span>
-          <Rating size={18} rating={4.2} />
-          <span>(13.1k)</span>
+      <div className='p-4 flex-1'>
+        <Link href={`/products/${slugify(title)}`}>
+          <h3 className='text-lg font-semibold text-gray-900 mb-1 line-clamp-4 text-balance hover:underline'>
+            {title}
+          </h3>
+        </Link>
+
+        <div className='flex items-center justify-between'>
+          <div>
+            <div className='text-sm text-gray-500'>
+              {brand} • {model}
+            </div>
+            <div className='text-xl font-bold text-gray-900 mt-1'>${price}</div>
+          </div>
+          {isCheckout ||
+            isPayment ||
+            isOrderConfirmed ||
+            isCart ||
+            isWishes || (
+              <div className='absolute bottom-4 right-4'>
+                <AddToCartButton itemId={id} />
+              </div>
+            )}
         </div>
-        <span className='font-bold text-dark-text text-xl'>
-          <strong className='font-normal text-sm'>$</strong>
-          {price}
-        </span>
         {(isCheckout || isPayment || isOrderConfirmed) && (
           <span className='text-xl'>x{qty}</span>
         )}
