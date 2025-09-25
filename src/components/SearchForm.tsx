fdcf32643d1812b57574fd6e01c6674070382f4d
Jose Vigo <josevigodev@gmail.com>
2025-09-25 10:53:58 -0400

    style: update SearchForm component for improved layout and consistency

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -26,12 +26,21 @@ export function SearchForm() {
   return (
     <form
       onSubmit={handleSubmit}
-      className='rounded-sm bg-amber-50 flex items-center overflow-hidden col-span-3 lg:order-1 lg:w-8/9 xl:w-full'
+      className='flex items-center pr-2 bg-gray-100 text-gray-700 border-2 border-transparent transition-all duration-200 outline-1 outline-transparent focus-within:border-gray-500 rounded-full col-span-3 overflow-hidden lg:order-1 lg:w-8/9 xl:w-full group search'
     >
+      <button
+        aria-label='search'
+        title='search'
+        className='text-black p-2 pl-3 cursor-pointer bg-transparent transition-all duration-200 hover:bg-black hover:text-white group-focus-within:bg-gray-300'
+        type='submit'
+      >
+        <SearchIcon />
+      </button>
       <input
         data-test='search-input'
-        className='w-full p-1 px-2 focus-visible:outline-0 placeholder:text-placeholder'
+        className='w-full p-2 px-2 focus-visible:outline-0 placeholder:text-placeholder'
         type='text'
+        required
         placeholder='Search'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
@@ -40,19 +49,12 @@ export function SearchForm() {
         <button
           onClick={handleClearSearch}
           aria-label='empty input'
-          className='px-2 text-placeholder cursor-pointer'
+          className='p-1 text-white cursor-pointer transition-colors duration-200 bg-gray-400 rounded-full hover:bg-gray-950 form'
           type='button'
         >
-          <XIcon className='size-4' />
+          <XIcon className='size-4 stroke-3' />
         </button>
       )}
-      <button
-        aria-label='search'
-        className='text-light-text p-1 bg-black cursor-pointer'
-        type='submit'
-      >
-        <SearchIcon />
-      </button>
     </form>
   );
 }
