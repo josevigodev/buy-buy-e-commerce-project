9d5a801a1bb8082250fa65bab3eaabd812cb4366
Jose Vigo <josevigodev@gmail.com>
2025-09-25 19:54:14 -0400

    style: adjust text size in PopUp component for improved readability

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const PopUp: React.FC<Props> = ({ text }) => {
   return (
-    <p className='fixed top-15 left-0 right-0 mx-auto w-fit px-4 py-2 bg-green-700 text-white text-md font-semibold rounded-xl shadow-xl pointer-events-none md:text-lg md:px-6 md-py-4 showPopUpFrame'>
+    <p className='fixed top-15 left-0 right-0 mx-auto w-fit px-4 py-2 bg-green-700 text-white text-sm font-semibold rounded-xl shadow-xl pointer-events-none md:text-md md:px-6 md-py-4 showPopUpFrame'>
       {text}
     </p>
   );
