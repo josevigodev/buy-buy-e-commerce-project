6029457d6ebc441ee796952af7311b8dc15ea14d
Jose Vigo <josevigodev@gmail.com>
2025-09-25 19:55:08 -0400

    style: refactor ProductCase component to remove unnecessary Link wrapper and streamline rendering of ProductCard

--- src/components/ProductCase.tsx
+++ src/components/ProductCase.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link';
 import { ProductCard } from './ProductCard';
 import { Product } from '@/types/fakeStoreApi';
-import { slugify } from '@/utils/slugify';
 import { useSyncFilters } from '@/hooks/useSyncFilters';
 
 interface Props {
@@ -11,15 +9,9 @@ interface Props {
 export function ProductCase({ filteredProducts }: Props) {
   useSyncFilters();
   return (
-    <section className='grid gap-2 lg:grid-cols-4 flex-1 mt-5 lg:pl-3'>
+    <section className='gap-2 grid grid-cols-2 lg:grid-cols-4 flex-1 mt-5 lg:pl-3'>
       {filteredProducts.map((item) => (
-        <Link
-          href={`/products/${slugify(item.title)}`}
-          key={item.id}
-          className='border-1 border-product-frame flex h-fit'
-        >
-          <ProductCard {...item} />
-        </Link>
+        <ProductCard key={item.id} {...item} />
       ))}
     </section>
   );
