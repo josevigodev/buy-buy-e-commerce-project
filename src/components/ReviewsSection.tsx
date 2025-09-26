d9ed78b03cdb52f5dba9472203ae0b79896cb665
Jose Vigo <josevigodev@gmail.com>
2025-09-26 14:05:09 -0400

    style: update styles of main title

--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -4,13 +4,13 @@ import { Review } from './Review';
 export function ReviewsSection() {
   return (
     <section className='relative py-10 overflow-hidden mx-auto mt-20 md:mt-28 pb-40'>
-      <h2 className='text-2xl lg:text-3xl text-center mb-10 font-semibold text-gray-100'>
+      <h2 className='text-2xl lg:text-5xl text-center mb-10 font-semibold text-gray-100'>
         What people think about us
       </h2>
 
       <ul className='flex gap-3.5 reviews-scroll'>
         {[...reviews, ...reviews].map((review, i) => (
-          <li key={`${review.id}-${i}`} className={`w-full`}>
+          <li key={`${review.id}-${i}`}>
             <Review {...review} />
           </li>
         ))}
