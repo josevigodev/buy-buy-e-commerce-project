b3ee09b1f30ddeecd5913489c777c14e75b4b6fe
Jose Vigo <josevigodev@gmail.com>
2025-09-26 14:05:27 -0400

    update styles to match the main design

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -37,26 +37,26 @@ export function SignIn() {
           Welcome back
         </p>
         <div className='flex flex-col gap-5'>
-          <label htmlFor='email' className='text-xl font-bold text-gray-600'>
+          <label htmlFor='email' className='text-xl font-bold text-gray-500'>
             Email
-            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-gray-500 rounded-sm group text-lg font-normal'>
-              <Mail className='group-hover:fill-gray-950 group-focus-within:fill-gray-950 transition-all duration-200' />
+            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-yellow-500 rounded-sm group text-lg font-normal'>
+              <Mail className='group-hover:fill-yellow-500 group-focus-within:fill-yellow-500 transition-all duration-200 cursor-text text-yellow-600' />
               <input
                 data-test='email-input'
                 autoFocus
                 required
                 id='email'
                 type='email'
-                className=' text-gray-600 focus-visible:outline-0 w-full peer'
+                className=' text-gray-700 focus-visible:outline-0 w-full peer'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
           </label>
-          <label htmlFor='password' className='text-xl font-bold text-gray-600'>
+          <label htmlFor='password' className='text-xl font-bold text-gray-500'>
             Password
-            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-gray-500 rounded-sm group text-lg font-normal'>
-              <Lock className='group-hover:fill-gray-950 group-focus-within:fill-gray-950 transition-all duration-200' />
+            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-yellow-500 rounded-sm group text-lg font-normal'>
+              <Lock className='group-hover:fill-yellow-500 group-focus-within:fill-yellow-500 transition-all duration-200 cursor-text text-yellow-600' />
               <div className='relative w-full'>
                 <input
                   required
@@ -65,15 +65,15 @@ export function SignIn() {
                   onChange={(e) => setPassword(e.target.value)}
                   id='password'
                   type={showPassword ? 'text' : 'password'}
-                  className='text-gray-600 focus-visible:outline-0 w-full peer'
+                  className='text-gray-700 focus-visible:outline-0 w-full peer'
                 />
                 <button
                   type='button'
                   title={showPassword ? 'Hide password' : 'Show password'}
                   onClick={() => setShowPassword((prev) => !prev)}
-                  className='absolute top-1/6 right-3 text-gray-600 cursor-pointer'
+                  className='absolute -top-1 -right-1 rounded-full text-gray-800 p-2 cursor-pointer hover:bg-gray-300 transition-colors duration-250'
                 >
-                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                  {showPassword ? <EyeOff size={22} /> : <Eye size={22} />}
                 </button>
               </div>
             </div>
@@ -84,7 +84,7 @@ export function SignIn() {
           <button
             data-test='confirm-button'
             type='submit'
-            className='bg-dark-gray text-white p-3 font-semibold cursor-pointer rounded-sm'
+            className='bg-yellow-500 text-white p-3 font-semibold cursor-pointer rounded-sm text-lg hover:bg-yellow-400 transition-colors duration-250'
           >
             Log in
           </button>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -65,17 +65,17 @@ export function SignUp() {
         <div className='flex flex-col gap-5'>
           <label
             htmlFor='email'
-            className='text-xl font-bold text-gray-600 transition-all duration-300'
+            className='text-xl font-bold text-gray-500 transition-all duration-300'
           >
             Email
-            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-600 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-gray-600 rounded-sm group text-lg font-normal'>
-              <Mail className='group-hover:fill-gray-950 group-focus-within:fill-gray-950 transition-all duration-200' />
+            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-600 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-yellow-500 rounded-sm group text-lg font-normal'>
+              <Mail className='group-hover:fill-yellow-500 group-focus-within:fill-yellow-500 transition-all duration-200 text-yellow-600' />
               <input
                 data-test='email-input'
                 autoFocus
                 id='email'
                 type='email'
-                className=' text-gray-600 focus-visible:outline-0 w-full peer'
+                className=' text-gray-700 focus-visible:outline-0 w-full peer'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -88,11 +88,11 @@ export function SignUp() {
           </label>
           <label
             htmlFor='password'
-            className='text-xl font-bold text-gray-600 transition-all duration-300'
+            className='text-xl font-bold text-gray-500 transition-all duration-300'
           >
             Password
-            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-gray-500 rounded-sm group text-lg font-normal'>
-              <Lock className='group-hover:fill-gray-950 group-focus-within:fill-gray-950 transition-all duration-200' />
+            <div className='mt-2 flex items-center gap-2 bg-gray-100 text-gray-700 border-1 border-gray-300 p-2 px-3 transition-all duration-250 outline-transparent focus-within:border-yellow-500 rounded-sm group text-lg font-normal'>
+              <Lock className='group-hover:fill-yellow-500 group-focus-within:fill-yellow-500 transition-all duration-200 text-yellow-600' />
               <div className='relative w-full'>
                 <input
                   data-test='password-input'
@@ -100,15 +100,15 @@ export function SignUp() {
                   onChange={(e) => setPassword(e.target.value)}
                   id='password'
                   type={showPassword ? 'text' : 'password'}
-                  className='text-gray-600 focus-visible:outline-0 w-full peer'
+                  className='text-gray-700 focus-visible:outline-0 w-full peer'
                 />
                 <button
                   type='button'
                   title={showPassword ? 'Hide password' : 'Show password'}
                   onClick={() => setShowPassword((prev) => !prev)}
-                  className='absolute top-1/6 right-3 text-gray-600 cursor-pointer'
+                  className='absolute rounded-full -top-1 -right-1 hover:bg-gray-300 transition-colors duration-250 text-gray-600 p-2 cursor-pointer'
                 >
-                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                  {showPassword ? <EyeOff size={22} /> : <Eye size={22} />}
                 </button>
               </div>
             </div>
@@ -124,7 +124,7 @@ export function SignUp() {
           <button
             data-test='confirm-button'
             type='submit'
-            className='bg-dark-gray text-white p-3 font-semibold cursor-pointer rounded-sm'
+            className='bg-yellow-500 text-white p-3 font-semibold cursor-pointer rounded-sm text-lg hover:bg-yellow-400 transition-colors duration-250'
           >
             Sign up
           </button>
