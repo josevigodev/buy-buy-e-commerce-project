8a49805beaf85f928f9c5a9e868a64510f6b3e19
Jose Vigo <josevigodev@gmail.com>
2025-09-27 18:03:01 -0400

    add ux

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,33 +1,46 @@
-'use client';
-import { useState } from 'react';
-import { FiltersAside } from './FiltersAside';
+import { useFilterStore } from '@/store/filters';
+import { ActiveFilters } from './ActiveFilters';
+import { FilterIcon } from './Icons';
+import { XIcon } from 'lucide-react';
+interface Props {
+  productsFound: number;
+  onOpenFilterAction: (value: boolean) => void;
+}
 
-export function Filters() {
-  const [openFilter, setOpenFilter] = useState(false);
-
+export function Filters({ onOpenFilterAction, productsFound }: Props) {
+  const resetFilters = useFilterStore((state) => state.resetFilters);
   const handleOpenClick = () => {
-    setOpenFilter(true);
+    onOpenFilterAction(true);
   };
 
   return (
     <>
-      <aside className='flex items-center justify-end border-b-1 border-b-gray-400 lg:hidden sticky top-0 bg-white text-dark-text'>
-        <button className='flex items-center justify-center gap-1 py-2 px-4 cursor-pointer'>
-          <span>Price</span>
-          <span>i</span>
-        </button>
-        <button
-          onClick={handleOpenClick}
-          className='flex items-center justify-center gap-1 py-2 px-4 cursor-pointer'
-        >
-          <span>Filter</span>
-          <span>i</span>
-        </button>
+      <aside className='flex flex-col p-2 shadow-md sticky top-25 lg:top-16.5 bg-white text-dark-text z-5'>
+        <div className='flex items-center justify-between lg:px-4'>
+          <p className='font-bold text-lg text-nowrap select-none lg:text-xl'>
+            {productsFound} {productsFound <= 1 ? 'product' : 'products'} found
+          </p>
+
+          <div className='flex items-center gap-1 text-black'>
+            <button
+              onClick={() => resetFilters()}
+              className='flex items-center rounded-md bg-gray-200 cursor-pointer justify-center gap-1 py-1 px-2 sm:py-2 sm:px-4 font-bold hover:bg-gray-300 transition-colors duration-200'
+            >
+              <span>Clear all</span>
+              <XIcon className='size-5' />
+            </button>
+            <button
+              aria-label='open filters'
+              onClick={handleOpenClick}
+              className='flex bg-yellow-300 rounded-md cursor-pointer items-center justify-center gap-1 py-1 px-2 sm:py-2 sm:px-4 font-bold lg:hidden transition-colors duration-200 hover:bg-yellow-400'
+            >
+              <span className='hidden sm:inline'>Filter</span>
+              <FilterIcon className='size-6' />
+            </button>
+          </div>
+        </div>
+        <ActiveFilters />
       </aside>
-      <FiltersAside
-        openFilter={openFilter}
-        onOpenFilterAction={setOpenFilter}
-      />
     </>
   );
 }
--- src/components/FiltersAside.tsx
+++ src/components/FiltersAside.tsx
@@ -2,6 +2,9 @@
 import { XIcon } from './Icons';
 import { CategoryFilters } from './CategoryFilters';
 import { BrandFilters } from './BrandFilters';
+import { ColorFilters } from './ColorFilters';
+import { useFilterStore } from '@/store/filters';
+import { useEffect, useState } from 'react';
 
 interface Props {
   openFilter: boolean;
@@ -9,6 +12,15 @@ interface Props {
 }
 
 export function FiltersAside({ openFilter, onOpenFilterAction }: Props) {
+  const filters = useFilterStore((state) => state.filters);
+  const setFilters = useFilterStore((state) => state.setFilters);
+
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    setPrice(filters.minPrice);
+  }, [filters.minPrice]);
+
   const handleCloseFilter = () => {
     onOpenFilterAction(false);
   };
@@ -16,20 +28,37 @@ export function FiltersAside({ openFilter, onOpenFilterAction }: Props) {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 w-full h-dvh z-40 flex transition-transform duration-500 ease-out lg:w-fit lg:z-0 lg:border-r-1 lg:border-r-gray-400 lg:static ${
+        className={`fixed top-0 left-0 w-full z-30 flex transition-transform duration-500 ease-out lg:w-fit lg:z-0 lg:static ${
           openFilter ? 'translate-x' : '-translate-x-full lg:translate-x-0'
         }`}
       >
-        <aside className=' w-[300px] h-full p-2.5 bg-white'>
-          <h2 className='text-center text-xl mb-4 font-semibold text-dark-text'>
-            Filters
-          </h2>
-          <section className='flex flex-col gap-4'>
+        <aside
+          style={{
+            scrollbarWidth: 'thin',
+          }}
+          className='overflow-y-scroll h-dvh lg:h-auto lg:overflow-auto w-[300px] p-2.5 pt-6 bg-white'
+        >
+          <section className='flex flex-col gap-9'>
             <BrandFilters />
             <CategoryFilters />
+            <ColorFilters />
             <div>
-              <h3 className='text-lg'>Price range</h3>
-              <small>aqui va el price range</small>
+              <h3 className='font-bold uppercase mb-2 text-lg px-2 w-full text-dark-text text-start'>
+                Minimum price
+              </h3>
+              <div className='flex items-center gap-2'>
+                <input
+                  className='h-2 bg-gray-300 cursor-pointer rounded-full w-full appearance-none'
+                  onChange={(e) =>
+                    setFilters({ key: 'minPrice', value: e.target.value })
+                  }
+                  min={0}
+                  max={1000}
+                  type='range'
+                  value={filters.minPrice}
+                />
+                <small className='text-lg'>${filters.minPrice}</small>
+              </div>
             </div>
           </section>
         </aside>
@@ -43,7 +72,7 @@ export function FiltersAside({ openFilter, onOpenFilterAction }: Props) {
       </div>
       {openFilter && (
         <div
-          className={`w-full h-dvh z-30 fixed top-0 left-0 bg-alpha lg:hidden`}
+          className={`w-full h-dvh z-20 fixed top-0 left-0 bg-alpha lg:hidden`}
         ></div>
       )}
     </>
