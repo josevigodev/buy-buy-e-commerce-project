f21ee3d941feff9785d1253f15c298efc9eb3756
Jose Vigo <josevigodev@gmail.com>
2025-09-28 12:42:08 -0400

    export type

--- src/store/filters.ts
+++ src/store/filters.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-interface Filters {
+export interface Filters {
   category: string | null;
   color: string | null;
   brand: string[];
