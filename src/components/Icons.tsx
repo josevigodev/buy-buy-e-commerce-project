8259e4e012582a586e5a736732134e22c96443a0
Jose Vigo <josevigodev@gmail.com>
2025-09-29 10:08:41 -0400

    add icons and finish side bar with go-to links

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -439,3 +439,132 @@ export const Trash: React.FC<Props> = ({ className }) => {
     </svg>
   );
 };
+export const Phone: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z' />
+      <path d='M11 4h2' />
+      <path d='M12 17v.01' />
+    </svg>
+  );
+};
+export const Game: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M12 5h3.5a5 5 0 0 1 0 10h-5.5l-4.015 4.227a2.3 2.3 0 0 1 -3.923 -2.035l1.634 -8.173a5 5 0 0 1 4.904 -4.019h3.4z' />
+      <path d='M14 15l4.07 4.284a2.3 2.3 0 0 0 3.925 -2.023l-1.6 -8.232' />
+      <path d='M8 9v2' />
+      <path d='M7 10h2' />
+      <path d='M14 10h2' />
+    </svg>
+  );
+};
+export const TV: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z' />
+      <path d='M16 3l-4 4l-4 -4' />
+    </svg>
+  );
+};
+export const Headset: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M4 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z' />
+      <path d='M15 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z' />
+      <path d='M4 15v-3a8 8 0 0 1 16 0v3' />
+    </svg>
+  );
+};
+export const Home: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M5 12l-2 0l9 -9l9 9l-2 0' />
+      <path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' />
+      <path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' />
+    </svg>
+  );
+};
+export const Market: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M3 21l18 0' />
+      <path d='M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4' />
+      <path d='M5 21l0 -10.15' />
+      <path d='M19 21l0 -10.15' />
+      <path d='M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4' />
+    </svg>
+  );
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,55 @@
-import { XIcon } from './Icons';
+import Link from 'next/link';
+import {
+  Game,
+  HeaderHeartIcon,
+  Headset,
+  HeartIcon,
+  Home,
+  Market,
+  Phone,
+  ShoppingCartIcon,
+  TV,
+  UserIcon,
+  XIcon,
+} from './Icons';
 import { LogInButton } from './LogInButton';
+import { useAuthStore } from '@/store/user';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   openSide: boolean;
   closeSideBarAction: (value: boolean) => void;
 }
 
+const categories = [
+  {
+    name: 'gaming',
+    icon: <Game />,
+  },
+  {
+    name: 'mobile',
+    icon: <Phone />,
+  },
+  {
+    name: 'audio',
+    icon: <Headset />,
+  },
+  {
+    name: 'tv',
+    icon: <TV />,
+  },
+];
+
 export function SideBar({ openSide, closeSideBarAction }: Props) {
+  const user = useAuthStore((state) => state.user);
   const handleClick = () => {
     closeSideBarAction(false);
   };
+
+  const isHome = usePathname() === '/';
+  const isMarket = usePathname().includes('/marcket');
+  const isCart = usePathname() === '/cart';
+  const isWishes = usePathname() === '/wishes';
 
   return (
     <>
@@ -18,9 +58,119 @@ export function SideBar({ openSide, closeSideBarAction }: Props) {
           openSide ? 'translate-x' : '-translate-x-full'
         }`}
       >
-        <aside className=' bg-light-text w-[300px] h-full p-2.5'>
-          <h2 className='text-2xl font-bold text-dark-text'>Menu</h2>
-          <LogInButton />
+        <aside className=' bg-light-text w-[300px] h-full p-2.5 flex flex-col'>
+          {user ? (
+            <Link
+              onClick={handleClick}
+              href='/my-account'
+              className='flex gap-2 items-center font-semibold text-lg text-dark-text p-2 px-1 pb-4 border-b-1 border-b-gray-300'
+            >
+              <span>
+                <UserIcon className='p-2 rounded-full border-2 size-10' />
+              </span>
+              <span className='flex flex-col'>
+                <small className='text-xl'>Hello!</small>
+                <small className='text-gray-600'>{user.email}</small>
+              </span>
+            </Link>
+          ) : (
+            <Link
+              onClick={handleClick}
+              href='/log-in'
+              className='flex gap-2 items-center font-semibold text-lg text-dark-text p-2 px-1 pb-4 border-b-1 border-b-gray-300'
+            >
+              <span>
+                <UserIcon className='p-2 rounded-full border-2 size-10' />
+              </span>
+              <span>Welcome! Sign in</span>
+            </Link>
+          )}
+
+          <nav className='mt-4 h-full'>
+            <ul className='flex flex-col gap-2'>
+              <li onClick={handleClick}>
+                <Link
+                  href='/'
+                  className={`flex gap-2 items-center font-semibold text-lg rounded-sm p-2 px-3 ${
+                    isHome
+                      ? 'bg-dark-gray text-white hover:bg-dark-gray'
+                      : 'text-dark-text hover:bg-gray-300 transition duration-100'
+                  }`}
+                >
+                  <span>
+                    <Home />
+                  </span>
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  href='/marcket'
+                  className={`flex gap-2 items-center font-semibold text-lg rounded-sm p-2 px-3 ${
+                    isMarket
+                      ? 'bg-dark-gray text-white hover:bg-dark-gray'
+                      : 'text-dark-text hover:bg-gray-300 transition duration-100'
+                  }`}
+                >
+                  <span>
+                    <Market />
+                  </span>
+                  <span>Market</span>
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  href='/cart'
+                  className={`flex gap-2 items-center font-semibold text-lg rounded-sm p-2 px-3 ${
+                    isCart
+                      ? 'bg-dark-gray text-white hover:bg-dark-gray'
+                      : 'text-dark-text hover:bg-gray-300 transition duration-100'
+                  }`}
+                >
+                  <span>
+                    <ShoppingCartIcon />
+                  </span>
+                  <span>Cart</span>
+                </Link>
+              </li>
+              <li onClick={handleClick}>
+                <Link
+                  href='/wishes'
+                  className={`flex gap-2 items-center font-semibold text-lg rounded-sm p-2 px-3 ${
+                    isWishes
+                      ? 'bg-dark-gray text-white hover:bg-dark-gray'
+                      : 'text-dark-text hover:bg-gray-300 transition duration-100'
+                  }`}
+                >
+                  <span>
+                    <HeaderHeartIcon />
+                  </span>
+                  <span>Wishes</span>
+                </Link>
+              </li>
+              {categories.map((category) => (
+                <li onClick={handleClick} key={category.name}>
+                  <Link
+                    href={`/marcket?category=${category.name}`}
+                    className='flex gap-2 items-center font-semibold text-lg text-dark-text rounded-sm p-2 px-3 hover:bg-gray-300 transition duration-100'
+                  >
+                    <span>{category.icon}</span>
+                    <span className='first-letter:uppercase'>
+                      {category.name}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <footer className='border-t-1 border-t-gray-300 pt-3'>
+            <h2 className='text-4xl lg:text-6xl font-bold text-dark-text select-none'>
+              BUY-BUY.
+            </h2>
+            <p className='mb-4 text-center font-normal text-blue-gray-900 md:mb-0'>
+              &copy; 2025 . All Rights Reserved.
+            </p>
+          </footer>
         </aside>
         <button
           aria-label='close menu'
