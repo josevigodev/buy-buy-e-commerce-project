27bb0beb99b2205eb244c4404d860734f6ccc6a2
Jose Vigo <josevigodev@gmail.com>
2025-10-04 13:40:54 -0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
   creator: 'Jose Vigo',
   keywords: ['buy', 'shop', 'electronics'],
   metadataBase: new URL('https://buy-buy-e-commerce-project.vercel.app/'),
+  icons: '/favicon.png',
   openGraph: {
     title: 'BUY-BUY',
     description:
