957305236409c7fc4cab7d687e642c39d2abe288
Jose Vigo <josevigodev@gmail.com>
2025-10-04 13:41:04 -0400

    change reviews user pictures

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Rating } from './Rating';
 import Profile from '../../public/computer.png';
+import { UserIcon } from './Icons';
 
 interface Props {
   user: string;
@@ -13,13 +14,7 @@ export const Review: React.FC<Props> = ({ user, comment, rating }) => {
     <figure className='flex flex-col w-md shrink-0 bg-[#48e2] rounded-2xl p-5 gap-2.5 hover:scale-105 hover:shadow-lg hover:shadow-yellow-300 transition-all duration-150 select-none'>
       <div className='flex flex-row items-center justify-between'>
         <div className='flex flex-row items-center gap-1.5'>
-          <Image
-            className='rounded-full w-auto h-auto object-center object-cover'
-            width={32}
-            height={32}
-            alt={`${user}'s profile picture`}
-            src={Profile}
-          />
+          <UserIcon className='rounded-full text-white bg-gray-600 p-2 size-10' />
           <div className='flex flex-col'>
             <figcaption className='text-sm font-medium text-white'>
               {user}
